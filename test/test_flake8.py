bb36bb1386d87869b713acd1cdfeb08d16f0d291
Matsura Kota <s24c1110qm@s.chibakoudai.jp>
2026-01-10 00:34:44 +0900

    Configure flake8 args in test code to handle build artifacts

--- test/test_flake8.py
+++ test/test_flake8.py
@@ -8,7 +8,12 @@ import pytest
 @pytest.mark.flake8
 @pytest.mark.linter
 def test_flake8():
-    rc, errors = main_with_errors()
+    args = [
+        '--max-line-length=120',
+        '--exclude=build,lib,.git,__pycache__,.venv'
+    ]
+    
+    rc, errors = main_with_errors(argv=args)
     assert rc == 0, \
         'Found %d code style errors / warnings:\n' % len(errors) + \
         '\n'.join(errors)
