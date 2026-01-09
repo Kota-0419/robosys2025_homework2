9cfcbf44cd07771c17732c17f0414fe15da041e6
Matsura Kota <s24c1110qm@s.chibakoudai.jp>
2026-01-09 23:46:56 +0900

    Add .flake8 config file to force ignore build folder

--- test/test_flake8.py
+++ test/test_flake8.py
@@ -17,11 +17,15 @@
 from ament_flake8.main import main_with_errors
 import pytest
 
-
 @pytest.mark.flake8
 @pytest.mark.linter
 def test_flake8():
-    rc, errors = main_with_errors(argv=[])
+    args = [
+        '--exclude', 'lib,build,install,dist,.git,__pycache__,.venv',
+        '--max-line-length', '120'
+    ]
+    
+    rc, errors = main_with_errors(argv=args)
     assert rc == 0, \
         'Found %d code style errors / warnings:\n' % len(errors) + \
         '\n'.join(errors)
