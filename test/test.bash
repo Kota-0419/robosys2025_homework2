5bbd601261b840ec959759dff3eca268fc237d24
Matsura Kota <s24c1110qm@s.chibakoudai.jp>
2026-01-18 22:05:41 +0900

    Add a command

--- test/test.bash
+++ test/test.bash
@@ -1,29 +1,35 @@
-#!/bin/bash
-# SPDX-FileCopyrightText: 2025 Kota Matsura <s24c1110qm@s.chibakoudai.jp>
+#!/bin/bash -xv
+# SPDX-FileCopyrightText: 2025 Kota Matsura
 # SPDX-License-Identifier: BSD-3-Clause
 
-dir=~
-[ "$1" != "" ] && dir="$1"
+ng () {
+      echo ${1}行目が違うよ
+      res=1
+}
 
-cd $dir/ros2_ws
-colcon build
+res=0
+
+# あなたの環境（Humble）に合わせて変更
+source /opt/ros/humble/setup.bash
+
+# パッケージをビルド（あなたのパッケージ名を指定）
+colcon build --symlink-install --packages-select robosys2025_homework2 || ng "$LINENO"
 source install/setup.bash
 
-ros2 run robosys2025_homework2 sensor &
-NODE_PID=$!
+# ノードを10秒間実行して、出力をログファイルに保存
+# （以前のログで ros2 run robosys2025_homework2 talker を実行していたため）
+timeout 10 ros2 run robosys2025_homework2 talker > /tmp/robosys2025_homework2.log 2>&1 || true
 
-sleep 5
+# ログを表示（デバッグ用）
+cat /tmp/robosys2025_homework2.log
 
-timeout 5 ros2 topic echo /sensor > /tmp/output.log
+# ログの中に "Publishing" という文字が含まれているかカウント
+# （ログ出力例: [INFO] ...: Publishing: 22 C）
+count=$(grep -c "Publishing" /tmp/robosys2025_homework2.log)
 
-cat /tmp/output.log
+# 1回以上出力されていればOK
+[ "$count" -ge 1 ] || ng "$LINENO"
 
-if grep -q "[0-9]" /tmp/output.log; then
-    echo "Test Passed: Output matches"
-    kill $NODE_PID
-    exit 0
-else
-    echo "Test Failed: Output does not match"
-    kill $NODE_PID
-    exit 1
-fi
+[ "$res" = 0 ] && echo OK
+
+exit $res
